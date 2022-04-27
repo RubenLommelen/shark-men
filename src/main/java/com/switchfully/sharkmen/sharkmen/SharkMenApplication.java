52ce1b483ffee80a2c64ecb608f9c289af5244c8
Ruben Lommelen <ruben.lommelen@gmail.com>
2022-04-27 15:12:36 +0200

    initial commit

---
+++ src/main/java/com/switchfully/sharkmen/sharkmen/SharkMenApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.sharkmen.sharkmen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkMenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharkMenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchfully/sharkmen/sharkmen/SharkMenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.sharkmen.sharkmen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharkMenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
