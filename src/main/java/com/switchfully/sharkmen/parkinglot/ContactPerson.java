7cfca2883b19ef32beba9a253637a35c0e1a762f
Herbert Coenen <coenenherbert@gmail.com>
2022-04-28 15:01:48 +0200

    PL1 - added first name and last name

--- src/main/java/com/switchfully/sharkmen/parkinglot/ContactPerson.java
+++ src/main/java/com/switchfully/sharkmen/parkinglot/ContactPerson.java
@@ -4,15 +4,17 @@ import com.switchfully.sharkmen.infrastructure.Address;
 
 public class ContactPerson {
     private final long id;
-    private final String name;
+    private final String firstName;
+    private final String lastName;
     private String mobilePhoneNumber;
     private String phoneNumber;
     private String email;
     private final Address address;
 
-    public ContactPerson(long id, String name, String mobilePhoneNumber, String phoneNumber, String email, Address address) {
+    public ContactPerson(long id, String firstName, String lastName, String mobilePhoneNumber, String phoneNumber, String email, Address address) {
         this.id = id;
-        this.name = name;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.mobilePhoneNumber = mobilePhoneNumber;
         this.phoneNumber = phoneNumber;
         this.email = email;
