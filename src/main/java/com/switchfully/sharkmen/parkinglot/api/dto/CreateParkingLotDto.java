2f27db2da7a0cc3fb8ac5cc126cc2f0f60bb4f2f
Ruben Lommelen <ruben.lommelen@gmail.com>
2022-04-29 14:47:49 +0200

    PL1: Added validation + tests

--- src/main/java/com/switchfully/sharkmen/parkinglot/api/dto/CreateParkingLotDto.java
+++ src/main/java/com/switchfully/sharkmen/parkinglot/api/dto/CreateParkingLotDto.java
@@ -3,11 +3,24 @@ package com.switchfully.sharkmen.parkinglot.api.dto;
 import com.switchfully.sharkmen.infrastructure.api.dto.CreateAddressDto;
 import com.switchfully.sharkmen.parkinglot.Category;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class CreateParkingLotDto {
+    @NotBlank(message = "name is blank or null")
     private final String name;
+    @NotNull(message = "category is blank or null")
     private final Category category;
+    @Min(value = 0)
+    @NotNull(message = "capacity is null")
     private final int capacity;
+    @Valid
+    @NotNull(message = "createContactPersonDto is null")
     private final CreateContactPersonDto createContactPersonDto;
+    @Valid
+    @NotNull(message = "createAddressDto is null")
     private final CreateAddressDto createAddressDto;
 
     public CreateParkingLotDto(String name, Category category, int capacity, CreateContactPersonDto createContactPersonDto, CreateAddressDto createAddressDto) {
