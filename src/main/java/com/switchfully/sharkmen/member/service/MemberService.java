1e5a48fa9558b386536edb9527b088de45b502dc
Alperen2112 <a.alperen@outlook.com>
2022-04-29 09:53:19 +0200

    ME1 added check for email address format

--- src/main/java/com/switchfully/sharkmen/member/service/MemberService.java
+++ src/main/java/com/switchfully/sharkmen/member/service/MemberService.java
@@ -7,10 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Pattern;
+
 @Service
 public class MemberService {
 
-    private Logger memberServiceLogger = LoggerFactory.getLogger(MemberService.class);
+    private final Logger memberServiceLogger = LoggerFactory.getLogger(MemberService.class);
 
     private final MemberMapper memberMapper;
 
@@ -20,7 +22,22 @@ public class MemberService {
 
     public MemberDto registerMember(CreateMemberDto createMemberDto) {
         Member memberToRegister = memberMapper.ToMember(createMemberDto);
+        if (!isEmailFormValid(memberToRegister.getEmailAddress())) {
+            memberServiceLogger.info("Wrong e-mail address format" + memberToRegister.getEmailAddress());
+            throw new IllegalArgumentException("Please provide a correct E-mail address");
+        }
         memberServiceLogger.info("Registering a member" + memberToRegister.getFirstName());
         return memberMapper.ToDto(memberToRegister);
     }
+
+    private boolean isEmailFormValid(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        return pat.matcher(email).matches();
+    }
+
 }
