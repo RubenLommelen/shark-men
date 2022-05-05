cf12f1b9d2d693998af0f789888cb92b02a20464
Jeremy Alen <alen_jeremy@hotmail.com>
2022-05-05 15:42:26 +0200

    Refactoring - Automatically applied A clean to the unused import and for the format

---
+++ src/main/java/com/switchfully/sharkmen/infrastructure/service/AddressMapper.java
@@ -0,0 +1,39 @@
+package com.switchfully.sharkmen.infrastructure.service;
+
+import com.switchfully.sharkmen.infrastructure.api.dto.AddressDto;
+import com.switchfully.sharkmen.infrastructure.api.dto.CreateAddressDto;
+import com.switchfully.sharkmen.infrastructure.domain.Address;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AddressMapper {
+
+    private final Logger addressMapperLogger = LoggerFactory.getLogger(AddressMapper.class);
+
+    private final PostalCodeMapper postalCodeMapper;
+
+    public AddressMapper(PostalCodeMapper postalCodeMapper) {
+        this.postalCodeMapper = postalCodeMapper;
+    }
+
+
+    public Address toAddress(CreateAddressDto createAddressDto) {
+        addressMapperLogger.info("CreateAddressDto conversion to Address");
+        return new Address(
+                createAddressDto.getStreetName(),
+                createAddressDto.getStreetNumber(),
+                postalCodeMapper.toPostalCode(createAddressDto.getPostalCode())
+        );
+    }
+
+    public AddressDto toDto(Address address) {
+        addressMapperLogger.info("Address conversion to AddressDto");
+        return new AddressDto(
+                address.getStreetName(),
+                address.getStreetNumber(),
+                postalCodeMapper.toDto(address.getPostalCode())
+        );
+    }
+}
